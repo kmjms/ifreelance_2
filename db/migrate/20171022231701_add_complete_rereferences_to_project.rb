9f8d05f856b9f59cbf37e3229601bcda199eda40
juligarji <juliangarzon.j@gmail.com>
2017-10-22 18:27:44 -0500

    cambio de ref de projectos

--- db/migrate/20171022231701_add_complete_rereferences_to_project.rb
+++ db/migrate/20171022231701_add_complete_rereferences_to_project.rb
@@ -1,8 +1,8 @@
 class AddCompleteRereferencesToProject < ActiveRecord::Migration[5.1]
   def change
-    add_reference :projects, :clients, foreign_key: true
-    add_reference :projects, :freelances, foreign_key: true
-    add_reference :projects, :states, foreign_key: true
-    add_reference :projects, :type_projects, foreign_key: true
+    add_reference :projects, :client, foreign_key: true
+    add_reference :projects, :freelance, foreign_key: true
+    add_reference :projects, :state, foreign_key: true
+    add_reference :projects, :type_project, foreign_key: true
   end
 end
